decbd02ee96c47bf6ea7153ca226dbe017ee1de0
Victor Hugo <victorferrarihugo@hotmail.com>
2024-08-17 13:36:50 -0300

    Finalização do Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+/*Importações Angular Material*/
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -7,11 +13,10 @@ import {MatInputModule} from '@angular/material/input';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-/*Importações do Angular*/
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { RestritoRoutingModule } from './restrito/restrito-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
@@ -21,7 +26,6 @@ import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-p
 import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
 import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
 import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
-
 
 @NgModule({
   declarations: [
@@ -39,14 +43,19 @@ import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.co
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule, 
+    BrowserAnimationsModule,
+    MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
     MatInputModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RestritoRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/Produto.model.ts
+++ src/app/models/Produto.model.ts
@@ -1,15 +1,15 @@
-export class Produto {
+export class Produto{
     id: number = 0;
     produto: string = "";
     descricao: string = "";
     foto: string = "";
     preco: number = 0;
 
-    constructor(id:number, produto:string, descricao:string, foto:string, preco:number) {
+    constructor(id: number, produto: string, descricao: string, foto:string, preco: number){
         this.id = id;
         this.produto = produto;
         this.descricao = descricao;
         this.foto = foto;
-        this.preco = preco;
+        this.preco =preco;
     }
 }
